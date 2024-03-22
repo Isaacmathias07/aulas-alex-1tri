93761bb9eaee43d3aca51d009fc8c44d40c9c9d4
Isaacmathias07 <133376387+Isaacmathias07@users.noreply.github.com>
2024-03-22 09:51:49 -0300

    Add files via upload

---
+++ arrays-alura2/divindo-sala.js
@@ -0,0 +1,31 @@
+const alunos = [
+    "Pietro",
+    "Cauã",
+    "Renan",
+    "Isaac",
+    "Felipe",
+    "Livia",
+    "Luis F",
+    "João V",
+    "Carlos",
+    "Gustavo L",
+    "Gilberto",
+    "Aluro",
+    "Calabreso",
+    "Ednaldo Pereira",
+    "Dona Quiaba",
+    "Dona Samambaia",
+    "Mouso",
+    "Alinho",
+    "KukaBeludo",
+    "20Comer70Correr",
+    "Takamassa Nomuro",
+    "Kevin Mamar",
+    "Sujiro Kimimame",
+];
+
+const sala1 = alunos.slice(0,10);
+const sala2 = alunos.slice(10);
+
+console.log(sala1);
+console.log(sala2);
